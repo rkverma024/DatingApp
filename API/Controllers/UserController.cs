f7811b6373edf50ab81ddf4ea76230e923e336d0
Rohit Kumar Verma <rverma@aakashinformatics.com>
2023-05-17 17:39:30 +0530

    Add project files.

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")] // /api/users
+    public class UserController : Controller
+    {
+        public UserController(DataContext context)
+        {
+        
+        }
+    }
+}
