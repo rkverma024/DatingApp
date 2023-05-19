7811dab9434e3193b0ade4330c6091943868f8c1
Rohit Kumar Verma <rverma@aakashinformatics.com>
2023-05-19 11:46:12 +0530

    Api Login & Registration

--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => 
 {
@@ -14,6 +18,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
 builder.Services.AddCors();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
